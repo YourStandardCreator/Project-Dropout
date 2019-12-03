808ed5cada670710559a0fb3df46f8cb0fa87bf8
YourStandardCreator <58487291+YourStandardCreator@users.noreply.github.com>
2019-12-03 15:20:17 -0500

    Add files via upload

---
+++ CAS Project/Project-Dropout/Project-Dropout/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Project-Dropout
+//
+//  Created by Damian on 2019-12-03.
+//  Copyright © 2019 United dropouts of America. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
